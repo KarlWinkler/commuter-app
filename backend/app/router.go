cbdc99ebdb00cf3e98debaf561964cc12835426f
karlWinkler <karlw200@gmail.com>
2025-01-01 18:00:11 -0700

    add post request on submission

--- backend/app/router.go
+++ backend/app/router.go
@@ -3,13 +3,18 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	ap "github.com/karlwinkler/web-server/api"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+    AllowOrigins: "http://localhost:3030",
+    AllowHeaders: "Origin, Content-Type, Accept",
+}))
 
 	api := app.Group("/")
-	api.Post("/", ap.Submit)
+	api.Post("/submit", ap.Submit)
 
 	app.Listen(":3001")
 }
--- frontend/src/routes/log-trip/index.svelte
+++ frontend/src/routes/log-trip/index.svelte
@@ -1,3 +1,24 @@
-<from>
-  <h1>Hello, World!</h1>
-</from>
+<script lang="ts">
+  let name = $state("")
+  let world = $state("World")
+
+  const handleSubmit = async (e: Event) => {
+    world = name
+    const response = await fetch('http://localhost:3001/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({"name": name}),
+    })
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <h1>Hello, {world}!</h1>
+
+  <label>Name</label>
+  <input type="text" bind:value={name} />
+
+  <button type="submit">Log Trip</button>
+</form>
